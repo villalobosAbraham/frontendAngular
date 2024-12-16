1047421ee0a75823232e88b7dae2f8940f0c14ee
Abraham <abrahamvillalobos3656@gmail.com>
2024-12-16 01:17:47 -0700

    por si acaso
    antes de instalar select2

--- src/app/sistema/libros/libros.component.ts
+++ src/app/sistema/libros/libros.component.ts
@@ -5,7 +5,10 @@ import { Router } from '@angular/router';
 import { datosGeneralesEncapsulado } from '../../shared/interfaces/datos-generales';
 import { SweetAlertService } from '../../services/sweet-alert.service';
 import { ApiService } from '../../services/api.service';
+import { FormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 import Swal from 'sweetalert2';
+import { windowWhen } from 'rxjs';
 
 declare var $: any;
 
@@ -27,16 +30,32 @@ interface Libro {
   14 : string,
   15 : number,
 }
+
+interface Autor {
+  0 : number,
+  1 : string,
+  2 : string,
+  3 : string,
+}
 @Component({
   selector: 'app-libros',
   standalone : true,
-  imports: [BarraSistemaComponent],
+  imports: [BarraSistemaComponent, FormsModule],
   templateUrl: './libros.component.html',
   styleUrl: './libros.component.css'
 })
 export class LibrosComponent {
   constructor(private Router : Router, private AlmacenamientoLocalService : AlmacenamientoLocalService, private SweetAlertService : SweetAlertService, private ApiService : ApiService) {}
   tabla : any = "";
+  titulo : string = "";
+  paginas : any = "";
+  fechaPublicacion : string = "";
+  sinopsis : string = "";
+  precioBase : any = 0;
+  descuento : any = 0;
+  iva : any = 0;
+  total : any = 0;
+  
 
   ngOnInit() {
     let almacenamientoLocal = this.AlmacenamientoLocalService.obtenerAlmacenamientoLocal("clave");
@@ -57,7 +76,10 @@ export class LibrosComponent {
       "autoWidth": true,
       "scrollY": "",
     });
+
+    $("#autor").select2();
     this.obtenerLibros();
+    this.obtenerAutoresActivos();
   }
 
   obtenerLibros() {
@@ -137,6 +159,29 @@ export class LibrosComponent {
     imagen.style.cssText = "max-height : 150px";
 
     return imagen;
+  }
+
+  obtenerAutoresActivos() {
+    let datosGenerales = this.prepararDatosGeneralesSoloToken();
+
+    this.ApiService.post('ADMObtenerAutoresActivos/', datosGenerales).subscribe(
+      (response : any) => {
+        if (typeof response === 'boolean') {
+          return;
+        } 
+        let autores : Autor[] = response;
+        this.AlmacenamientoLocalService.actualizarToken(datosGenerales.datosGenerales);
+        this.AlmacenamientoLocalService.guardarAlmacenamientoLocal('clave', datosGenerales.datosGenerales); // Para objetos
+        autores.forEach((autor: Autor) => {
+          let optionAutor = $("<option>").attr("value", autor[0]).text(autor[1] + " " + autor[2] + " " + autor[3]);
+          $("#autor").append(optionAutor);
+        });
+        $("#autor").val("-1").trigger("change");
+
+      },
+      (error) => {
+      }
+    );
   }
 
   confirmarHabilitarLibro() : any {
@@ -270,6 +315,128 @@ export class LibrosComponent {
   }
 
   abrirModalAgregarLibro() {
+    // $("#")
     $("#modal").modal("show");
+  }
+
+  abrirModalConfirmarAgregar() {
+    if (!this.comprobarDatosAgregar()) {
+      return;
+    }
+    Swal.fire({
+      title: "Estas Seguro?",
+      text: "Confirmar Agregar Libro",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Agregar"
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.agregarLibro();
+      }
+    });
+  }
+
+  comprobarDatosAgregar() {
+    if (this.titulo == "") {
+      this.SweetAlertService.mensajeError("Titulo Vacio");
+      return false;
+    } else if (this.paginas <= 0 || this.paginas == "") {
+      this.SweetAlertService.mensajeError("Paginas Erroneas");
+      return false;
+    } else if (!this.fechaPublicacion || this.fechaPublicacion == "") {
+      this.SweetAlertService.mensajeError("Fecha de Publicacion Erronea");
+      return false;
+    } else if (this.sinopsis == "") {
+      this.SweetAlertService.mensajeError("Falta Sinopsis");
+      return false;
+    } else if (this.precioBase <= 0 || this.paginas == "") {
+      this.SweetAlertService.mensajeError("Precio Base Incorrecto");
+      return false;
+    } else if (this.descuento <= -1 || this.descuento > this.precioBase) {
+      this.SweetAlertService.mensajeError("Descuento Incorrecto");
+      return false;
+    } else if (this.iva >= this.precioBase) {
+      this.SweetAlertService.mensajeError("Fallo de Calculo de Iva, Reiniciando Pagina");
+      setTimeout(() => {
+        window.location.reload();
+      }, 3000);
+    } else if (this.total <= -1) {
+      this.SweetAlertService.mensajeError("Fallo de Calculo de Total, Reiniciando Pagina");
+      setTimeout(() => {
+        window.location.reload();
+      }, 3000);
+    }
+
+    return true;
+  }
+
+  agregarLibro() {
+    let datosGenerales = this.prepararDatosGeneralesAgregarLibro();
+
+    this.SweetAlertService.cargando();
+    this.ApiService.post('ADMAgregarLibroCatalogo/', datosGenerales).subscribe(
+      (response : any) => {
+        if (response == false) {
+          this.SweetAlertService.close();
+          this.SweetAlertService.mensajeError("Fallo al Agregar el Libro");
+          setTimeout(() => {
+            $("#modal").modal("hide");
+            this.obtenerLibros();
+          }, 3000);
+          return;
+        } 
+        this.AlmacenamientoLocalService.actualizarToken(datosGenerales.datosGenerales.token);
+        this.AlmacenamientoLocalService.guardarAlmacenamientoLocal('clave', datosGenerales.datosGenerales.token); // Para objetos
+        this.SweetAlertService.close();
+        this.SweetAlertService.mensajeFunciono("Libro Agregado con Exito");
+        setTimeout(() => {
+          $("#modal").modal("hide");
+          this.obtenerLibros();
+        }, 3000);
+      },
+      (error) => {
+        this.SweetAlertService.close();
+        this.SweetAlertService.mensajeError("Fallo de Conexion al Servidor");
+        setTimeout(() => {
+          $("#modal").modal("hide");
+          this.obtenerLibros();
+        }, 3000);
+      }
+    );
+  }
+
+  prepararDatosGeneralesAgregarLibro() {
+    let token = this.AlmacenamientoLocalService.obtenerAlmacenamientoLocal("clave");
+    token = this.AlmacenamientoLocalService.actualizarToken(token);
+    let idGenero = $("#genero").val();
+    let fecha = $("#fechaPublicacion").val().split("/").reverse().join("");
+    let idIdioma = $("#idioma").val();
+    let idEditorial = $("#editorial").val();
+
+    let datos = {
+      titulo : this.titulo,
+      precio : this.precioBase,
+      descuento : this.descuento,
+      iva : this.iva,
+      idGenero : idGenero,
+      fechaPublicacion : fecha,
+      sinopsis : this.sinopsis,
+      paginas : this.paginas,
+      idIdioma : idIdioma,
+      idEditorial : idEditorial,
+    }
+
+    let datosGenerales = {
+      datosGenerales : datos,
+      token : token,
+    }
+
+    let datosGeneralesEncapsulados : datosGeneralesEncapsulado = {
+      datosGenerales : datosGenerales
+    };
+
+    return datosGeneralesEncapsulados;
   }
 }
